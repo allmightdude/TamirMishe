b5f3f7b81bb99d4b2761c4e70c8f172fa5a55ee6
allmightdude <sabzi_f@yahoo.com>
2023-11-12 00:13:24 +0330

    feat : add btn component

---
+++ app/assets/helpers/object.js
@@ -0,0 +1,9 @@
+/**
+ * Get object keys by list
+ * ***********************************/
+export const keys = Object.keys;
+
+/**
+ * Extend Object
+ * ***********************************/
+export const assign = Object.assign;
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,51 @@
-const {gulp , dest , task , watch , parallel , series, src } = require('gulp');
-const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));
+const { gulp, dest, task, watch, parallel, series, src } = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass")(require("sass"));
+const env = require("gulp-env");
 
-
-function serve(){
-    browserSync.init({
-        server : {
-            baseDir : "./app"
-        }
-    })
-    watch(['./app/scss/**/*.scss'] , scss)
-    watch('./app/css/*.css').on('change' , browserSync.reload)
-    watch('./app/js/*.js').on('change' , browserSync.reload)
-    watch("./app/**/*.html").on('change', browserSync.reload)
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./app",
+    },
+  });
+  watch(["./app/scss/**/*.scss"], scss);
+  watch("./app/css/*.css").on("change", browserSync.reload);
+  watch("./app/js/*.js").on("change", browserSync.reload);
+  watch("./app/**/*.html").on("change", browserSync.reload);
 }
 
-function scss(done){
-    src('./app/scss/**/*.scss')
-    .pipe(sass().on('error' , sass.logError))
-    .pipe(dest('./app/css/'))
-    done();
+function scss(done) {
+  src("./app/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./app/css/"));
+  done();
 }
 
-task('serve' , serve);
-task('scss' , scss);
+function setEnv() {
+  // read the env file
+  env.set({ file: "env.json" });
+  // set the web url based on the node environment
+  switch (process.env.NODE_ENV) {
+    case "dev":
+      process.env.WEBAPP_URL = process.env.dev.WEBAPP_URL;
+      break;
+    case "prod":
+      process.env.WEBAPP_URL = process.env.prod.WEBAPP_URL;
+      break;
+    case "prod:test":
+      process.env.WEBAPP_URL = process.env["prod:test"].WEBAPP_URL;
+      break;
+    case "prod:demo":
+      process.env.WEBAPP_URL = process.env["prod:demo"].WEBAPP_URL;
+      break;
+    default:
+      process.env.WEBAPP_URL = "http://localhost:3000";
+  }
+}
 
-task('default' , parallel(serve , scss));
+task("serve", serve);
+task("scss", scss);
+task("setEnv", setEnv);
+
+task("default", parallel(setEnv , serve, scss));
